e26ece81d1eaf24fc91717e5ee6ae7ed0b74a835
Elliot <easnyder@uaschools.org>
2022-11-04 15:36:47 -0400

    init

---
+++ src/main/java/frc/robot/subsystems/PnumaticsSubsystem.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public class PnumaticsSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  private DoubleSolenoid test = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,4,5);
+  public PnumaticsSubsystem() {}
+  public void extend()
+  {
+    test.set(DoubleSolenoid.Value.kForward);
+  }
+  public void retract()
+  {
+    test.set(DoubleSolenoid.Value.kReverse);
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
